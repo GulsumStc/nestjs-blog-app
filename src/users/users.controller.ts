9eb211b3a97926cc37329c4cb6269e00d4454aef
GulsumStc <121710851+GulsumStc@users.noreply.github.com>
2024-08-19 21:21:54 +0300

    chore: tested usage of request components

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,20 +1,33 @@
-import { Controller, Get, Post, Patch, Delete, Put } from '@nestjs/common';
+import { Controller, Get, Post, Patch, Delete, Put, Param, Query, Body, Headers, Ip } from '@nestjs/common';
 
 // http://localhost:8000/users
 @Controller('users')
 export class UsersController {
 
-
-  @Get()
-  public getUser() {
+  /* 
+   the @Param() decorator is used to capture route parameters from the URL.
+  */
+  @Get('/:id/:optional?')
+  public getUser(@Param('id') id: any, @Query('limit') limit: any) {
+    console.log(id);
+    console.log(limit);
     return "Get User";
   }
 
 
   @Post()
-  public createUsers() {
-    return "Create User";
+  public createUsers
+    (
+    @Body() request: any,
+    @Headers() headers: any,
+    @Ip() ip: any  // ip contains the client's IP address
+    )
+  {
+    console.log(ip)
+    console.log(headers)
+    return request;
   }
+  // instead of @Body() we can use @Req() from express
 
 
   @Put()
