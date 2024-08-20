8f7f74188e1b70d453d5b724f51c71298f0de39a
GulsumStc <121710851+GulsumStc@users.noreply.github.com>
2024-08-20 15:23:58 +0300

    feat:CreateUserDto is used in controller class with ValidationPipe

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,5 @@
-import { Controller, Get, Post, Patch, Delete, Put, Param, Query, Body, Headers, Ip, ParseIntPipe, DefaultValuePipe} from '@nestjs/common';
+import { Controller, Get, Post, Patch, Delete, Put, Param, Query, Body, Headers, Ip, ParseIntPipe, DefaultValuePipe, ValidationPipe } from '@nestjs/common';
+import { CreateUserDto } from './dtos/create-user.dto';
 
 // http://localhost:8000/users
 @Controller('users')
@@ -12,24 +13,19 @@ export class UsersController {
     // ParseIntPipe is used to convert the id to numberand it is not a decorator
     @Param('id', ParseIntPipe) id: number | undefined, // undefined because id is optional
     @Query('limit', new DefaultValuePipe(10), ParseIntPipe) limit: number,
-    @Query('page',new DefaultValuePipe(1), ParseIntPipe) page: number
+    @Query('page', new DefaultValuePipe(1), ParseIntPipe) page: number
   ) {
-    console.log( limit);
-    console.log( page);
+    console.log(limit);
+    console.log(page);
     return "Get User";
   }
 
 
   @Post()
   public createUsers(
-
-    @Body() request: any,
-    @Headers() headers: any,
-    @Ip() ip: any  // ip contains the client's IP address
-  ) {
-    console.log(ip)
-    console.log(headers)
-    return request;
+    @Body(new ValidationPipe()) createUserDto: CreateUserDto)
+  {
+    return createUserDto;
   }
   // instead of @Body() we can use @Req() from express
 
