8c347f60935afa17c7d7df210095961b7afa0257
GulsumStc <121710851+GulsumStc@users.noreply.github.com>
2024-08-20 13:44:03 +0300

    chore:validating Params with Built-in pipes which is ParseIntPipe

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Patch, Delete, Put, Param, Query, Body, Headers, Ip } from '@nestjs/common';
+import { Controller, Get, Post, Patch, Delete, Put, Param, Query, Body, Headers, Ip, ParseIntPipe } from '@nestjs/common';
 
 // http://localhost:8000/users
 @Controller('users')
@@ -7,9 +7,12 @@ export class UsersController {
   /* 
    the @Param() decorator is used to capture route parameters from the URL.
   */
-  @Get('/:id/:optional?')
-  public getUser(@Param('id') id: any, @Query('limit') limit: any) {
-    console.log(id);
+  @Get('/:id?') // if you use optional for param or query, ParseIntPipe will throw an error. 
+  public getUser(
+    // ParseIntPipe is used to convert the id to numberand it is not a decorator
+    @Param('id', ParseIntPipe) id: number | undefined, // undefined because id is optional
+    @Query('limit') limit: any) {
+    console.log(typeof id);
     console.log(limit);
     return "Get User";
   }
