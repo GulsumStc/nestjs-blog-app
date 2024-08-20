55a3cfaa05146ff677637937a3f9b09c95e3fdaf
GulsumStc <121710851+GulsumStc@users.noreply.github.com>
2024-08-20 14:04:10 +0300

    feat: added and tested new pipe usage (DefaultValuePipe())

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Patch, Delete, Put, Param, Query, Body, Headers, Ip, ParseIntPipe } from '@nestjs/common';
+import { Controller, Get, Post, Patch, Delete, Put, Param, Query, Body, Headers, Ip, ParseIntPipe, DefaultValuePipe} from '@nestjs/common';
 
 // http://localhost:8000/users
 @Controller('users')
@@ -7,25 +7,26 @@ export class UsersController {
   /* 
    the @Param() decorator is used to capture route parameters from the URL.
   */
-  @Get('/:id?') // if you use optional for param or query, ParseIntPipe will throw an error. 
+  @Get('/:id?')
   public getUser(
     // ParseIntPipe is used to convert the id to numberand it is not a decorator
     @Param('id', ParseIntPipe) id: number | undefined, // undefined because id is optional
-    @Query('limit') limit: any) {
-    console.log(typeof id);
-    console.log(limit);
+    @Query('limit', new DefaultValuePipe(10), ParseIntPipe) limit: number,
+    @Query('page',new DefaultValuePipe(1), ParseIntPipe) page: number
+  ) {
+    console.log( limit);
+    console.log( page);
     return "Get User";
   }
 
 
   @Post()
-  public createUsers
-    (
+  public createUsers(
+
     @Body() request: any,
     @Headers() headers: any,
     @Ip() ip: any  // ip contains the client's IP address
-    )
-  {
+  ) {
     console.log(ip)
     console.log(headers)
     return request;
