c726bf27c4a368a8823084f6707426cfa51c45ab
GulsumStc <121710851+GulsumStc@users.noreply.github.com>
2024-08-22 22:12:45 +0300

    feat: added some validations to create post DTO

--- src/posts/dtos/create-post.dto.ts
+++ src/posts/dtos/create-post.dto.ts
@@ -1,19 +1,51 @@
+import { IsArray, IsEnum, IsISO8601, isISO8601, IsJSON, IsNotEmpty, IsOptional, IsString, IsUrl, Matches, MinLength } from "class-validator";
 import { PostStatus } from "../enums/postStatus.enum";
 import { PostType } from "../enums/postType.enum";
 
 export class CreatePostDto {
 
 
+  @IsString()
+  @MinLength(4)
+  @IsNotEmpty()
   title: string;
-  postType: PostType
-  slug: string;
-  status: PostStatus;
-  content?: string;
-  schema?: string;
-  featuredImageUrl?: string;
-  publishOn?: Date;
-  tags: string[];
 
+  @IsEnum(PostType)
+  @IsNotEmpty()
+  postType: PostType
+
+  @IsString()
+  @IsNotEmpty()
+  @Matches(/^[a-z0-9]+(?:-[a-z0-9]+)*$/, { message: 'A slug can only contain lowercase letters, numbers, and dashes' })
+  slug: string;
+
+  @IsEnum(PostStatus)
+  @IsNotEmpty()
+  status: PostStatus;
+
+  @IsString()
+  @IsOptional()
+  content?: string;
+
+  @IsOptional()
+  @IsJSON()
+  schema?: string;
+
+  @IsOptional()
+  @IsUrl()
+  featuredImageUrl?: string;
+
+  @IsISO8601() // YYYY-MM-DD
+  @IsOptional()
+  publishOn?: Date;
+
+  @IsOptional()
+  @IsArray()
+  @IsString({ each: true })
+  @MinLength(3, { each: true })
+  tags?: string[];
+
+  
   metaOptions: [{key: 'sidebarEnabled', value: true}];
   
 
