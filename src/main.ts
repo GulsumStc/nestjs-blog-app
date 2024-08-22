6db3d85b0f26ad9eaeffbf0de40397201be8177f
GulsumStc <121710851+GulsumStc@users.noreply.github.com>
2024-08-22 12:58:55 +0300

    feat: integrated Swagger for API documentation in main.ts with custom configuration - Configured Swagger using DocumentBuilder - Set API title, description, and version - Added server details and terms of service - Generated and set up Swagger documentation at /api endpoint

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() { // this funtion bootstraps the application
   const app = await NestFactory.create(AppModule);
@@ -10,6 +11,25 @@ async function bootstrap() { // this funtion bootstraps the application
     forbidNonWhitelisted: true, // forbids non whitelisted properties and throws an error
     transform: true,// it transform the request to an instance of the dto class after validation
   }));
+
+
+  /* Swagger configuration */
+
+  const config = new DocumentBuilder()
+    .setTitle('Blog App API')
+    .setDescription('Use the base api url http://localhost:8000')
+    .setTermsOfService('http://localhost:8000/terms-of-service')
+    .setLicense('MIT License', 'https://github.com/git/git-scm.com/blob/main/MIT-LICENSE.txt')
+    .addServer('http://localhost:8000', 'localhost')
+    .setVersion('1.0')
+    .build();
+  // instantiate document
+  const document = SwaggerModule.createDocument(app, config);
+  //use domument to complate setup
+  SwaggerModule.setup('api', app, document);
+
+
+
   await app.listen(8000);
 }
 bootstrap();
