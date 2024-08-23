176d62c6cb90bcea688bc5bce3595a9180579dc9
GulsumStc <121710851+GulsumStc@users.noreply.github.com>
2024-08-23 22:36:44 +0300

    feat: TypeOrmModule injected and default configration complated successfuly

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,22 @@ import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { PostsModule } from './posts/posts.module';
 import { AuthModule } from './auth/auth.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 
 @Module({
-  imports: [UsersModule, PostsModule, AuthModule],
+  imports: [UsersModule, PostsModule, AuthModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      entities: [],
+      synchronize: true,
+      host: 'localhost',
+      port: 5432,
+      username: 'username',
+      password: 'password',
+      database: 'nestjs-blog-app',
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
