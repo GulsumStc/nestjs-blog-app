c8ab37045f8e76034b21279168748219933906e5
GulsumStc <121710851+GulsumStc@users.noreply.github.com>
2024-09-01 11:51:23 +0300

    feat(Transaction): Implement createMany method with transaction handling - Added createMany method to handle the creation of multiple users within a transaction - Utilized a QueryRunner instance to manage database connection and transaction lifecycle. - Successfully commits the transaction if all users are created; otherwise, rolls back in case of failure. - Ensures database connection is released in both success and failure scenarios

--- src/users/providers/users.service.ts
+++ src/users/providers/users.service.ts
@@ -1,6 +1,6 @@
 import { forwardRef, Inject, Injectable } from "@nestjs/common";
 import { GetUsersParamDto } from "../dtos/get-users-param.dto";
-import { Repository } from "typeorm";
+import { DataSource, Repository } from "typeorm";
 import { User } from "../user.entity";
 import { InjectRepository } from "@nestjs/typeorm";
 import { CreateUserDto } from "../dtos/create-user.dto";
@@ -23,7 +23,10 @@ export class UsersService {
     private userRepository: Repository<User>,
 
     @Inject(profileConfig.KEY)
-    private readonly privateConfigration:ConfigType<typeof profileConfig>,
+    private readonly privateConfigration: ConfigType<typeof profileConfig>,
+    
+     /* inject Datasource */
+    private readonly datasource: DataSource
   
   ) { }
 
@@ -82,4 +85,41 @@ export class UsersService {
     return user;
 
   }
+
+
+  public async createMany(createUserDto: CreateUserDto[]) {
+
+    let newUSers: User[] = [];
+
+    // create Querry Runner Instance
+      const queryRunner = this.datasource.createQueryRunner();
+    // Connect Query Runner to Database
+    await queryRunner.connect();
+    // Start Transaction
+    await queryRunner.startTransaction();
+    try {
+      
+      for (let user of createUserDto) { 
+        let newUser = queryRunner.manager.create(User, user); // create new user
+        let result = await queryRunner.manager.save(newUser); // save new user
+        newUSers.push(result); // push new user to array
+      }
+      // If successful, Commit 
+      await queryRunner.commitTransaction();// means  the transaction is commited to the db, it is successful transaction
+      
+    } catch (error) {
+      // If unsuccessful, Rollback 
+      await queryRunner.rollbackTransaction(); // even if a few user created, everthing is rolled back to initial state brfore you started off with the transaction
+
+    } finally {
+      // Release connection : close db connection
+      await queryRunner.release(); // no matter what happens, connection will be released
+      
+    }
+    
+    
+
+  }
+
+
 }
