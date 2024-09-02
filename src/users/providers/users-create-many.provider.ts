c00f1fec2289cdbe9bb2a417ce03c034a8a1bdbe
GulsumStc <121710851+GulsumStc@users.noreply.github.com>
2024-09-02 17:35:31 +0300

    feat:feat(user-creation): Added error handling for batch user creation - Added RequestTimeoutException to handle database connection failures. - Added ConflictException to handle transaction failures during batch user creation. - Updated UsersCreateManyProvider to provide detailed error messages in case of issues.

--- src/users/providers/users-create-many.provider.ts
+++ src/users/providers/users-create-many.provider.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable, RequestTimeoutException } from '@nestjs/common';
 import { CreateUserDto } from '../dtos/create-user.dto';
 import { User } from '../user.entity';
 import { DataSource } from 'typeorm';
@@ -17,13 +17,20 @@ export class UsersCreateManyProvider {
   public async createMany(createManyUsersDto: CreateManyUsersDto) {
 
     let newUSers: User[] = [];
-
     // create Querry Runner Instance
     const queryRunner = this.datasource.createQueryRunner();
-    // Connect Query Runner to Database
-    await queryRunner.connect();
-    // Start Transaction
-    await queryRunner.startTransaction();
+
+    try {
+      // Connect Query Runner to Database
+      await queryRunner.connect();
+      // Start Transaction
+      await queryRunner.startTransaction();
+      
+    } catch (error) {
+      throw new RequestTimeoutException('Could not connect to database');
+    }
+
+    
     try {
       
       for (let user of createManyUsersDto.users) {
@@ -38,10 +45,14 @@ export class UsersCreateManyProvider {
       // If unsuccessful, Rollback 
       await queryRunner.rollbackTransaction(); // even if a few user created, everthing is rolled back to initial state brfore you started off with the transaction
 
+      throw new ConflictException('Could not complate the transaction', {
+        description: String(error)
+      });
+
     } finally {
       // Release connection : close db connection
       await queryRunner.release(); // no matter what happens, connection will be released
-      
+      // no need to add try catch here but if you want to add it you can
     }
 
     return newUSers;
