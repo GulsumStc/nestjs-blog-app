96dfa140f3d83932fa4b3241f91ea54c148502bb
GulsumStc <121710851+GulsumStc@users.noreply.github.com>
2024-09-05 18:09:41 +0300

    feat(security): Implement password hashing and comparison using BcryptProvider

--- src/auth/providers/bcrypt.provider.ts
+++ src/auth/providers/bcrypt.provider.ts
@@ -1,14 +1,18 @@
 import { Injectable } from '@nestjs/common';
 import { HashingProvider } from './hashing.provider';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class BcryptProvider implements HashingProvider {
 
-  hashPassword(password: string | Buffer): Promise<string> {
-    throw new Error('Method not implemented.');
+  // Hash a given password with a generated salt
+  public async hashPassword(password: string | Buffer): Promise<string> {
+    const salt = await bcrypt.genSalt();  // Generate a salt
+    return bcrypt.hash(password, salt);   // Hash the password with the salt
   }
-  
-  comparePassword(plainTextPassword: string | Buffer, hashedPassword: string): Promise<boolean> {
-    throw new Error('Method not implemented.');
+
+  // Compare a plain text password with a hashed password
+  public comparePassword(plainTextPassword: string | Buffer, hashedPassword: string): Promise<boolean> {
+    return bcrypt.compare(plainTextPassword, hashedPassword);
   }
 }
