41955baab2b091e635fc2a971ed79bfbcb73f86b
GulsumStc <121710851+GulsumStc@users.noreply.github.com>
2024-09-05 22:38:13 +0300

    docs: Add explanatory comments to the abstract HashingProvider class to describe its purpose and usage

--- src/auth/providers/hashing.provider.ts
+++ src/auth/providers/hashing.provider.ts
@@ -1,5 +1,17 @@
 import { Injectable } from '@nestjs/common';
 
+/**
+ * Abstract class `HashingProvider` defines the contract for password hashing and comparison.
+ *
+ * By using an abstract class:
+ * 1. **Encapsulation of Logic:** You define the methods (`hashPassword` and `comparePassword`) that any concrete implementation must provide, without specifying how these methods should be implemented.
+ * 2. **Code Reusability:** Different hashing algorithms (like bcrypt, Argon2, etc.) can extend this class, providing their own implementation of the methods, while adhering to a common interface.
+ * 3. **Flexibility:** It allows you to easily switch between different hashing strategies by changing the concrete implementation without altering the rest of your application.
+ * 4. **Consistency:** Ensures that all concrete classes implementing `HashingProvider` will have the required methods, promoting a consistent approach to password management across your application.
+ *
+ * ! Example usage:
+ * - `BcryptProvider` or `Argon2Provider` can extend this class and provide the specific details of how passwords are hashed and compared.
+ */
 @Injectable()
 export abstract class HashingProvider {
 
