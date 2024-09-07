5a106cb4b761883868b5d9d111679e32dd538e10
GulsumStc <121710851+GulsumStc@users.noreply.github.com>
2024-09-07 12:42:28 +0300

    feat(jwt-Configration):Add JWT configuration to AuthModule and set environment variables - Configured JWT settings in AuthModule using jwtConfig and JwtModule.registerAsync. - Added JWT environment variables to env.development file. - created jwt.config.ts file

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -5,6 +5,9 @@ import { UsersModule } from 'src/users/users.module';
 import { HashingProvider } from './providers/hashing.provider';
 import { BcryptProvider } from './providers/bcrypt.provider';
 import { SignInProvider } from './providers/sign-in.provider';
+import { ConfigModule } from '@nestjs/config';
+import jwtConfig from './config/jwt.config';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
   controllers: [AuthController],
@@ -12,7 +15,12 @@ import { SignInProvider } from './providers/sign-in.provider';
     provide: HashingProvider,
     useClass: BcryptProvider
   }, SignInProvider],
-  imports: [forwardRef(() => UsersModule)],
+  imports: [
+    forwardRef(() => UsersModule),
+    ConfigModule.forFeature(jwtConfig), // now jwtConfig is available to this module  and can accessable via  help of @InjectConfig() decorator
+    JwtModule.registerAsync(jwtConfig.asProvider()) // asProvider helps you to avoid passing jwtConfig in every provider/ avoid writing extra boilerplate code  
+    // jwt module is injected into your application
+  ],
   exports: [AuthService, HashingProvider],
 })
 export class AuthModule {}
