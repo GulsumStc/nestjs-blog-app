d96e534f979b9639aa24dd58e51e51371d84e090
GulsumStc <121710851+GulsumStc@users.noreply.github.com>
2024-09-08 22:48:17 +0300

    feat(AccessTokenGuards): implemented AccessTokenGuards and created auth.constants.ts - Added AccessTokenGuard to validate JWT tokens and set user information in request. - Utilizes JwtService and configuration from jwtConfig to verify tokens. - Added helper method extractTokenFromHeader to retrieve tokens from the request headers. - Introduced REQUEST_USER_KEY for storing user information from the token payload. - Created  for defining constants used in authentication.

--- src/auth/guards/access-token/access-token.guard.ts
+++ src/auth/guards/access-token/access-token.guard.ts
@@ -1,8 +1,23 @@
-import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { CanActivate, ExecutionContext, Inject, Injectable, UnauthorizedException } from '@nestjs/common';
+import { ConfigType } from '@nestjs/config';
+import { JwtService } from '@nestjs/jwt';
 import { Observable } from 'rxjs';
+import jwtConfig from 'src/auth/config/jwt.config';
+import { Request } from 'express';
+import { access } from 'fs';
+import { REQUEST_USER_KEY } from 'src/auth/constant/auth.constants';
 
 @Injectable()
 export class AccessTokenGuard implements CanActivate {
+
+  constructor(
+
+    private readonly jwtService: JwtService,
+
+    @Inject(jwtConfig.KEY)
+    private readonly jwtConfigration: ConfigType<typeof jwtConfig>,
+    
+  ) { }
 
   /**
    * This method have to be implemented by all guards.
@@ -12,10 +27,45 @@ export class AccessTokenGuard implements CanActivate {
    * @param context The execution context of the request.
    * @returns A boolean indicating if the request is allowed or not.
    */
-  canActivate(
-    context: ExecutionContext,
-  ): boolean | Promise<boolean> | Observable<boolean> {
-    return true;
+  async canActivate(context: ExecutionContext,): Promise<boolean> {
+
+    // extract the request from the execution context
+    const request = context.switchToHttp().getRequest<Request>();
+
+    // extract the tokoen from the header
+    const token = this.extractTokenFromHeader(request);
+
+    // validate the token
+    try {
+      const payload = await this.jwtService// user ID and email in payload
+        .verifyAsync(token, this.jwtConfigration); 
+      
+      // set the payload in the request so, whenever i use access token i will be able to grab the user ID and email from the request
+      request[REQUEST_USER_KEY] = payload;
+
+    } catch {
+      throw new UnauthorizedException();
+    }
+
+      return true;
+
+  }
+
+
+
+  /**
+   * Helper method to extract the token from the request headers.
+   * If the token is valid, then it is returned, otherwise undefined is returned.
+   * @param request The request object.
+   * @returns The token or undefined if the token is invalid.
+   */
+  private extractTokenFromHeader(request: Request): string | undefined {
+    
+    // We expect the authorization header to be in the format of "Bearer <token>",
+    // so we split it and get the second part, which is the token.
+    const [type, token] = request.headers.authorization?.split(' ') ?? [];
+    // const [_, token] = request.headers.authorization?.split(' ') ?? [];
+    return type === 'Bearer' ? token : undefined;
   }
 
 
