099ba9daa12e82f643bdca38a258f4aaf360f805
GulsumStc <121710851+GulsumStc@users.noreply.github.com>
2024-09-16 23:04:02 +0300

    feat(auth): Implement 'ActiveUser' decorator and enhance JWT payload typing with 'ActiveUserData' - Created 'ActiveUser' custom decorator to easily access authenticated user data from the request object based on 'REQUEST_USER_KEY'. This decorator allows for retrieving specific fields like 'id' or 'email' - Introduced 'as ActiveUserData' type assertion to ensure strong typing when generating JWT tokens, aligning the token payload with 'ActiveUserData' structure.

--- src/auth/providers/sign-in.provider.ts
+++ src/auth/providers/sign-in.provider.ts
@@ -5,6 +5,7 @@ import { HashingProvider } from './hashing.provider';
 import { JwtService } from '@nestjs/jwt';
 import { ConfigType } from '@nestjs/config';
 import jwtConfig from '../config/jwt.config';
+import { ActiveUserData } from '../interfaces/active-user-data.interface';
 
 
 @Injectable()
@@ -51,7 +52,7 @@ export class SignInProvider {
       {
         sub: user.id,
         email: user.email
-      }, 
+      } as ActiveUserData, 
       {
         audience: this.jwtConfigration.audience,
         issuer: this.jwtConfigration.issuer,
