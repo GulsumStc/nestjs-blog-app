5534dc2d8e24042d9f8c7af322cc1112d97ad857
GulsumStc <121710851+GulsumStc@users.noreply.github.com>
2024-09-16 23:11:56 +0300

    chore(jsdoc): created jsdoc comment to @ActiceUSer decorator

--- src/auth/decorator/active-user.decorator.ts
+++ src/auth/decorator/active-user.decorator.ts
@@ -2,6 +2,14 @@ import { createParamDecorator, ExecutionContext } from "@nestjs/common";
 import { ActiveUserData } from "../interfaces/active-user-data.interface";
 import { REQUEST_USER_KEY } from "../constant/auth.constants";
 
+/**
+ * Custom NestJS decorator to access the authenticated user data from
+ * the request. The `REQUEST_USER_KEY` is set in the
+ * `AccessTokenGuard` when the user is authenticated.
+ * @param field Optional field of the user data to return.
+ * @returns The active user data or the user data with the
+ * specified field if provided.
+ */
 export const ActiveUser = createParamDecorator((field: keyof ActiveUserData | undefined, ctx: ExecutionContext) => {
 
   const request = ctx.switchToHttp().getRequest();
